7f245f2c2680706a0fb108a3914db49c91ad6b88
Zach Supalla <zsupalla@gmail.com>
2012-02-26 21:29:10 -0600

    Fixed some js syntax

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,22 +1,23 @@
-jQuery(document).ready(function($) {
-	$('a').click(function(e)) {
+$(document).ready(function() {
+	$('a').click(function(e) {
 		e.preventDefault();
 		toggleLights();
-	}
+	});
+	
+	var lightsOn = false;
+
+	function toggleLights() {
+		if (lightsOn) {
+			$.ajax('/devices/Elroy/turnOff', {
+				// More stuff here
+			});
+			lightsOn = false;
+		} else {
+			$.ajax('/devices/Elroy/turnOn', {
+				// More stuff here
+			});
+			lightsOn = true;
+		}
+	} 
 });
 
-var lightsOn = false;
-
-function toggleLights() {
-	if (lightsOn) {
-		$.ajax('/devices/Elroy/turnOff', {
-			// More stuff here
-		});
-		lightsOn = false;
-	} else {
-		$.ajax('/devices/Elroy/turnOn', {
-			// More stuff here
-		});
-		lightsOn = true;
-	}
-}
