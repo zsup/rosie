bbe99b1c4ff4af0da6e829ab319b7258827a2df7
Zach Supalla <zsupalla@gmail.com>
2012-03-02 10:47:56 -0600

    Changed to port 80

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get('/device/:id/:dothis', function(req, res){
 	}
 });
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
