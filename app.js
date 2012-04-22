165e3c5a5e37e03420eef68d85a0583e76b2bd70
keithrr <keith@rushrez.com>
2012-04-22 11:05:07 -0500

    first commit for multi-device stuff

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var http = require('http');
 
 // Store TCP connections in an array.
 var sockets = {};
+var intervals = {};
 
 // Helpers for processing TCP stream.
 var buffer = "";
@@ -26,9 +27,12 @@ var server;
 var express = require('express')
   , routes = require('./routes')
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(),
+	io = require('socket.io').listen(app);
 
 // Configuration
+io.set('log level', 1); 
+io.set('reconnect', false); 
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -49,23 +53,143 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+function ts() {
+	d = new Date().toTimeString()
+	index = d.indexOf(" GMT")
+	d = d.slice(0,index) + ": "
+	return d
+}
+
+function clog(msg) {
+	console.log(ts()+msg)
+};
+
 // Routing for the web-app
 
-app.get('/', routes.index);
+function loadDeviceInfo(req, res, next) {
+  req.devices = devices;
+  next();
+}
+
+app.get('/', loadDeviceInfo, routes.index);
+
 
 // Routing for http requests (API)
 app.get('/device/:id/:dothis', function(req, res){
 	var id = req.params.id;
 	var dothis = req.params.dothis;
-	console.log('HTTP request received for device ' + id + ' to ' + dothis);
+	clog('HTTP request received for device ' + id + ' to ' + dothis);
 	// If this device is connected, send it a message
-	if (sockets[id]) {
-		sockets[id].write(dothis + '\n');
+	if (devices[id]) {
+		switch(dothis) {
+			case "turnOn":
+				clog('sending turnOn to '+id)
+				devices[id].socket.write('turnOn\n');
+				break;
+			case "turnOff":
+				clog('sending turnOff to '+id)
+				devices[id].socket.write('turnOff\n');
+				break;
+			case "toggle":
+				if(devices[id].devicestatus=="On") {
+					clog('sending turnOff to '+id)
+					devices[id].socket.write('turnOff\n');
+					devices[id].devicestatus="Off";
+				}
+				else {
+					clog('sending turnOn to '+id)
+					devices[id].devicestatus="On";
+					devices[id].socket.write('turnOn\n');
+				};
+				updateStatus(devices[id]);
+				break;
+			case "flash":
+				// first toggle once
+				if(devices[id].devicestatus=="On") {
+					clog('sending turnOff to '+id)
+					devices[id].socket.write('turnOff\n');
+					devices[id].devicestatus="Off";
+				}
+				else {
+					clog('sending turnOn to '+id)
+					devices[id].devicestatus="On";
+					devices[id].socket.write('turnOn\n');
+				};
+				updateStatus(devices[id]);
+				
+
+				//then set flash timer
+				intervalID = setInterval(flashToggle, 750, id)
+				devices[id].flashstatus = "Flashing";
+				devices[id].flashID = intervalID;
+				updateFlashStatus(devices[id]);
+				break;
+			case "stopflash":
+				clearInterval(devices[id].flashID)
+				delete devices[id].flashID;
+				devices[id].flashstatus = "NotFlashing";
+				updateFlashStatus(devices[id]);
+				break;
+			default:
+				break;
+		}
 		res.send('Message sent.');
 	} else {
 		res.send('No device connected with that ID.');
 	}
 });
+
+
+function flashToggle(id) {
+	if(devices[id].devicestatus=="On") {
+		clog('sending turnOff to '+id+'(due to flashing)')
+		devices[id].socket.write('turnOff\n');
+		devices[id].devicestatus="Off";
+	}
+	else {
+		clog('sending turnOn to '+id+'(due to flashing)')
+		devices[id].devicestatus="On";
+		devices[id].socket.write('turnOn\n');
+	};
+	updateStatus(devices[id]);
+}
+
+function updateFlashStatus(device) {
+	clients.map(function(client) {
+			client.emit('flashstatuschange', {
+				devicename: device.devicename,
+				flashstatus: device.flashstatus
+			});
+	});
+};
+
+function updateStatus(device) {
+	clients.map(function(client) {
+			client.emit('statuschange', {
+				devicename: device.devicename,
+				devicestatus: device.devicestatus
+			});
+	});
+};
+
+function addDevice(device) {
+	clients.map(function(client) {
+			client.emit('adddevice', {
+				devicename: device.devicename,
+				devicestatus: device.devicestatus,
+				flashstatus: device.flashstatus
+			});
+	});
+};
+
+function removeDevice(device) {
+	clients.map(function(client) {
+			client.emit('removedevice', {devicename: device.devicename});
+	});
+};
+
+
+
 
 // Routing for a few redirects
 app.get('/demo', function(req, res){
@@ -87,39 +211,100 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 
 // Create the TCP server to communicate with the Arduino.
 server = net.createServer(function(socket) {
-	console.log('TCP client connected');
+	clog('TCP client connected');
 	socket.setEncoding('ascii');
 	socket.setKeepAlive(true);
 
-	socket.on('end', function() {
-		// delete sockets[socket.remoteAddress];
-		console.log('TCP client disconnected');
+	socket.devices = [];
+
+	socket.on('close', function() {
+		devicename = socket.devices.pop()
+		
+		while(devicename) {
+			clog("hereeeeee")
+			deleteDevice(devicename);
+			devicename = socket.devices.pop()
+		}
+		clog('TCP client disconnected');
 	});
 	
 	// Receive and parse incoming data
 	socket.on('data', function(chunk) {
 		buffer += chunk;
-		console.log("Message received: " + chunk + " End of message.");
-		
+		clog("Message received: " + chunk);
 		var separatorIndex = buffer.indexOf(separator);
 		var foundMessage = separatorIndex != -1;
 		
-		if (foundMessage) {
+		while(separatorIndex != -1) {
 			var message = buffer.slice(0, separatorIndex);
+			//clog("Found message: " + message);
 			process(message, socket);
 			buffer = buffer.slice(separatorIndex + 1);
-		}
+			separatorIndex = buffer.indexOf(separator);
+		};
 	});
 });
 
 // Fire up the TCP server bound to port 1307
 server.listen(1307, function() {
-	console.log('TCP server bound to port 1307');
+	clog('TCP server bound to port 1307');
 });
+
+devices = {};
+
+deleteDevice = function(devicename) {
+	clog("deleting" + devicename)
+	removeDevice(devices[devicename]);
+	delete devices[devicename];
+};
 
 // Process messages
 process = function (message, socket) {
 	message = message.trim();
-	console.log("Processing message: " + message);
-	sockets[message] = socket;
+	clog("Processing message: " + message);
+	//sockets[message] = socket;
+
+	var msgobj = JSON.parse(message);
+
+	devicename = msgobj.devicename
+	devicestatus = msgobj.devicestatus
+	flashstatus = "NotFlashing"
+
+	devices[devicename] = {
+		devicename: devicename,
+		devicestatus: devicestatus,
+		flashstatus: flashstatus,
+		socket: socket,
+	};
+
+	socket.devices.push(devicename);
+
+	addDevice({
+		devicename: devicename,
+		devicestatus: devicestatus,
+		flashstatus: flashstatus
+	});
+
+
+	// if we get a status
+	//devicename = devices.findkey(socket=socket) // returns "Elroy"
+	//devicename.status = status
 }
+
+clients = [];
+
+io.sockets.on('connection', function (iosocket) {
+	clients.push(iosocket);
+	for (var id in devices) {
+		device = devices[id];
+		clog("emitting to " + device.devicename);
+		iosocket.emit('statuschange', {
+				devicename: device.devicename,
+				devicestatus: device.devicestatus
+		});
+	};
+	iosocket.on('disconnect', function () {
+    	clients.splice(clients.indexOf(iosocket), 1)
+  	});
+});
+
--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,11 +1,98 @@
 $(document).ready(function() {
-	$('a').click(function(e) {
-		e.preventDefault();
-		toggleLights();
+
+	$('a.togbutton').click(bindToggle);
+	$('a.flashbutton').click(bindFlash);
+	$('a.stopflashbutton').click(bindStopFlash);
+
+	var socket = io.connect('http://localhost', {
+		reconnect: false
 	});
 
-	function toggleLights() {
-		$.ajax('/device/Elroy/toggle');
-	} 
+	socket.on('connect', function () {
+		$('.srvr').addClass('srvrok');
+	});
+	
+	socket.on('statuschange', function (statusobj) {
+		devicename = statusobj["devicename"];
+		devicestatus = statusobj["devicestatus"];
+		$('td[devicename="'+devicename+'"]').attr('devicestatus', devicestatus)
+	});
+	
+	socket.on('flashstatuschange', function (statusobj) {
+		devicename = statusobj["devicename"];
+		flashstatus = statusobj["flashstatus"];
+		if (flashstatus == "Flashing") {
+			fbutton = $('a.fbutton[devicename="'+devicename+'"]')
+			fbutton.removeClass('flashbutton');
+			fbutton.addClass('stopflashbutton');
+			fbutton.text("StopFlash");
+			fbutton.unbind('click');
+			fbutton.click(bindStopFlash)
+		}
+		else {
+			fbutton = $('a.fbutton[devicename="'+devicename+'"]')
+			fbutton.removeClass('stopflashbutton');
+			fbutton.addClass('flashbutton');
+			fbutton.text("Flash");
+			fbutton.unbind('click');
+			fbutton.click(bindFlash)
+		};
+	});
+
+	socket.on('adddevice', function (deviceobj) {
+		last = $('tr').last()
+    	var newrow = last.clone();
+    	
+    	devicon = newrow.find('td.devicon')
+    	devicon.attr('devicename', deviceobj.devicename)
+    	devicon.attr('devicestatus', deviceobj.devicestatus)
+    	devicon.text(deviceobj.devicename)
+    	
+    	togbutton = newrow.find('a.togbutton')
+    	togbutton.attr('devicename', deviceobj.devicename)
+    	togbutton.click(bindToggle)
+    	
+		fbutton = newrow.find('a.fbutton')
+    	fbutton.attr('devicename', deviceobj.devicename)
+    	if (deviceobj.flashstatus == "Flashing") {
+			fbutton.addClass('stopflashbutton');
+			fbutton.text("StopFlash")
+			fbutton.click(bindStopFlash)
+		}
+		else {
+			fbutton.addClass('flashbutton');
+			fbutton.text("Flash")
+			fbutton.click(bindFlash)
+		};
+
+    	last.before(newrow)
+	});
+
+	socket.on('removedevice', function (deviceobj) {
+		devicename = deviceobj["devicename"];
+		$('td[devicename="'+devicename+'"]').parent().remove()
+	});
+
+	socket.on('disconnect', function() {
+		$('body').css('background-color', "#f00");
+	})
+
 });
 
+function bindToggle(e) {
+	e.preventDefault();
+	devicename = $(this).attr('devicename');
+	$.ajax('/device/' + devicename + '/toggle');		
+};
+
+function bindFlash(e) {
+	e.preventDefault();
+	devicename = $(this).attr('devicename');
+	$.ajax('/device/' + devicename + '/flash');		
+};
+
+function bindStopFlash(e) {
+	e.preventDefault();
+	devicename = $(this).attr('devicename');
+	$.ajax('/device/' + devicename + '/stopflash');		
+};
