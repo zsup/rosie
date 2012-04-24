10c7217d0482892f9f283b2c8ee6a957cced1036
Zach Supalla <zsupalla@gmail.com>
2012-04-24 11:53:07 -0500

    Half-fixed the adddevice/removedevice functions

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -70,44 +70,60 @@ $(document).ready(function() {
 	});
 
 	socket.on('adddevice', function (deviceobj) {
-		var last = $('tr').last()
+		var last = $('.row').last();
     	var newrow = last.clone();
+
+		if (newRow.hasClass('clone')) {
+			newrow.removeClass('clone');
+		}
+		
+		newrow.attr('deviceid', deviceobj.deviceid);
+
+		var devtitle = newrow.find('.devtitle');
+		devtitle.text(deviceobj.deviceid);
+		
+		var devtype = newrow.find('.devtype');
+		devtype.text(deviceobj.devicetype);
     	
-    	var devicon = newrow.find('td.devicon')
-    	devicon.attr('deviceid', deviceobj.deviceid)
-    	devicon.attr('devicestatus', deviceobj.devicestatus)
-    	devicon.text(deviceobj.deviceid)
+    	var devicon = newrow.find('.devicon')
+    	devicon.attr('deviceid', deviceobj.deviceid);
+    	devicon.attr('devicestatus', deviceobj.devicestatus);
     	
     	var togbutton = newrow.find('a.togbutton')
     	togbutton.attr('deviceid', deviceobj.deviceid)
     	togbutton.click(bindToggle)
     	
 		var fbutton = newrow.find('a.fbutton')
-    	fbutton.attr('deviceid', deviceobj.deviceid)
+    	fbutton.attr('deviceid', deviceobj.deviceid);
     	if (deviceobj.flashstatus == "Flashing") {
-			fbutton.addClass('stopflashbutton');
-			fbutton.text("StopFlash")
-			fbutton.click(bindStopFlash)
+			if (newrow.hasClass('flashbutton')) {
+				fbutton.removeClass('flashbutton');
+				fbutton.addclass('stopflashbutton');
+				fbutton.addClass('active');
+			}
+			fbutton.click(bindStopFlash);
 		}
 		else {
-			fbutton.addClass('flashbutton');
-			fbutton.text("Flash")
-			fbutton.click(bindFlash)
+			if (newrow.hasClass('stopflashbutton')) {
+				fbutton.removeClass('stopflashbutton');
+				fbutton.addClass('flashbutton');
+				fbutton.removeClass('active');
+			}
+			fbutton.click(bindFlash);
 		};
 
 		var dimslider = newrow.find('.dimslider');
-		dimslider.attr('deviceid', deviceobj.deviceid)
+		dimslider.attr('deviceid', deviceobj.deviceid);
+		dimslider.attr('initval', deviceobj.dimval);
 		dimslider.slider(slideparam);
 
-		var dimvalp = newrow.find('.dimvalp');
-		dimvalp.attr('deviceid', deviceobj.deviceid)
-
-    	last.before(newrow)
+    	last.after(newrow);
+		newrow.show();
 	});
 
 	socket.on('removedevice', function (deviceobj) {
 		deviceid = deviceobj["deviceid"];
-		$('td[deviceid="'+deviceid+'"]').parent().remove()
+		$('.row.'deviceid).remove()
 	});
 
 	socket.on('disconnect', function() {
